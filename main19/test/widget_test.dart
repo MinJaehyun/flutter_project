88c892b095e5af0f475206991e151a2fe0168d97
MinJaehyun <43669992+MinJaehyun@users.noreply.github.com>
2023-06-13 12:52:28 +0900

    modified test.dart

--- main19/test/widget_test.dart
+++ main19/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:main19/main.dart';
+import 'package:main19/data_table/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
