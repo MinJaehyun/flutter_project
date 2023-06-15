f2e0a32c368aa06b3313151e7754371c9633a069
MinJaehyun <43669992+MinJaehyun@users.noreply.github.com>
2023-06-15 16:59:22 +0900

    updated main21.1.geolocator test

--- main21/lib/main.dart
+++ main21/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,6 +10,49 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      home: GetLocation(),
+    );
+  }
+}
+
+class GetLocation extends StatefulWidget {
+  const GetLocation({super.key});
+
+  @override
+  State<GetLocation> createState() => _GetLocationState();
+}
+
+class _GetLocationState extends State<GetLocation> {
+  void getLocation() async {
+    // note: 권한 요청
+    await Geolocator.requestPermission();
+    // note: 위도, 경도 가져오려면 관련 패키지 설정하기
+    Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+    print(position);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('GetLocation'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                // todo: 버튼 클릭 시, getLocation 정보를 가져온다
+                getLocation();
+              },
+              child: Text('GetLocation btn'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
